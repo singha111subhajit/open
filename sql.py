bc1140d4da8ad3a2d6c1f8c4a40f5913d7b4624f
subhajit <singha111subhajit@gmail.com>
2023-11-18 10:22:44 +0530

    ini

--- sql.py
+++ sql.py
@@ -1,6 +1,6 @@
 import pyodbc
 
-def execute_sql_script(server, database, username, password, script_path):
+def execute_sql_script(server, database, username, password, script_path, output_file_path):
     conn = None
     try:
         # Connect to the SQL Server
@@ -17,22 +17,25 @@ def execute_sql_script(server, database, username, password, script_path):
         with open(script_path, 'r') as script_file:
             sql_script = script_file.read()
 
-        # Execute the SQL script
-        cursor.execute(sql_script)
+        # Split the script into individual statements
+        sql_statements = [statement.strip() for statement in sql_script.split(";")]
 
-        # Commit the changes
-        conn.commit()
+        for statement in sql_statements:
+            if statement:
+                # Execute each statement
+                cursor.execute(statement)
 
-        print("SQL script executed successfully!")
+                # Check if the statement is a SELECT query
+                if cursor.description is not None:
+                    # Fetch all rows and store the results in a list
+                    rows = cursor.fetchall()
 
-        # Select all values from TestTable
-        select_query = "SELECT * FROM TestTable"
-        cursor.execute(select_query)
-        
-        # Fetch all rows and print the results
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
+                    # Write the results to a text file
+                    with open(output_file_path, 'a') as output_file:
+                        for row in rows:
+                            output_file.write(str(row) + '\n')
+
+        print(f"SQL script executed successfully! Results written to: {output_file_path}")
 
     except pyodbc.Error as e:
         print(f"Error executing SQL script: {e}")
@@ -42,11 +45,14 @@ def execute_sql_script(server, database, username, password, script_path):
         if conn:
             conn.close()
 
+
+
 # Example usage
 server = 'localhost,1433'
 database = 'master'  # Replace with your actual database name
 username = 'sa'
 password = 'YourPassword123!'  # Replace with your actual password
-script_path = '/home/subhajit/Desktop/paramount/test_script.sql'
+script_path = '/home/subhajit/Desktop/open/sql_1.sql'
+output_file_path = '/home/subhajit/Desktop/open/sql_script_results.txt'
 
-execute_sql_script(server, database, username, password, script_path)
+execute_sql_script(server, database, username, password, script_path, output_file_path)
